e869b4df406c5ae85e2c2d4d4ce911013a75b733
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-02-25 19:00:57 +0530

    simple addition

---
+++ basics/simpleadd.java
@@ -0,0 +1,11 @@
+package basics;
+public class simpleadd {
+    public static void main(String args[]){
+        int a =1;
+        int b =2;
+        int sum = a +b;
+        System.out.println(sum);
+    }
+}  
+//output:
+// 3
