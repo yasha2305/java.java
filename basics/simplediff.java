4c771e645fdb29e06deb8b5c5c51739ab380fd6b
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-02-26 18:35:02 +0530

    Simple difference(subtraction)

---
+++ basics/simplediff.java
@@ -0,0 +1,10 @@
+package basics;
+
+class simplediff {
+    public static void main(String arg[]) {
+        int a = 90;
+        int b = 76;
+        int diff = a - b;
+        System.out.println("difference=" + diff);
+    }
+}
