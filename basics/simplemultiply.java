05a9f9b38dc4f8717465ef968b099dfdd490d1e6
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-02-27 18:43:23 +0530

    simple  multiplication of two numbers

---
+++ basics/simplemultiply.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class simplemultiply {
+    public static void main(String arg[]) {
+        double a = 10006;
+        double b = 67;
+        double multi;
+        multi = a * b;
+        System.out.println("multiplication value of 10006 x 67 =" + multi);
+    }
+}
