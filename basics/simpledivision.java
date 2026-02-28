b1083f0dd5cc81a09cc839c5e740b72ebcc9c55a
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-02-28 18:15:45 +0530

    simple division

---
+++ basics/simpledivision.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class simpledivision {
+    public static void main(String arg[]) {
+        int a = 7689;
+        int b = 98;
+        float div;
+
+        div = a / b;
+        System.out.println("division=" + div);
+    }
+
+}
