98a761834d9f2c26b64b40ff3257cd7fe49974e9
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-03-02 18:07:16 +0530

    length of a string

---
+++ basics/stringfunc.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class stringfunc {
+    public static void main(String args[]) {
+        // Non - Primitive types
+        String name = "Yasha";
+        System.out.println(name.length());
+
+    }
+
+}
+//length of a string
