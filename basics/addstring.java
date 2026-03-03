df6140f05b0c6f6bdc52f1a91abe18746baded1c
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-03-03 17:56:02 +0530

    addition of two strings

---
+++ basics/addstring.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class addstring {
+    public static void main(String args[]) {
+        // Strings
+        // concatenate
+        String name1 = "Yasha";
+        String name2 = "Broken";
+        String name3 = name1 + name2;
+        System.out.println(name3);
+    }
+}
