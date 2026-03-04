07da0ae06c59345a30d57fe79b0d1b414ce38bdf
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-03-04 19:34:34 +0530

    character place value i.e . index:0 value stored Y

---
+++ basics/charplace.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class charplace {
+    public static void main(String args[]){
+        //Strings
+        //charAt
+        String name = "Yasha";
+        System.out.println(name.charAt(0));
+    }
+    
+}
