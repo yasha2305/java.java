0de231ae06a4f8194ff4a0fb4ae7953132073f22
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-03-05 19:37:35 +0530

    name letter  replace

---
+++ basics/namereplace.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class namereplace {
+    public static void main(String args[]){
+        //String 
+        //replace
+        String name = "Soumya";
+        String name2 = name.replace('o',  'a');
+        System.out.println(name2);
+
+    }
+
+}
