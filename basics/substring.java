f00428b0af6d73410ce60f128e39abbadf525ffd
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-03-06 18:25:23 +0530

    Substring

---
+++ basics/substring.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class substring {
+    public static void main(String args[]) {
+        // Strings
+        // substring
+        String name = "Yasha and Soumya";
+        System.out.println(name.substring(10, 16));
+    }
+
+}
