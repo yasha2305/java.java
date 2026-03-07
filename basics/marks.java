93029b45b94b40bc8f36803280e7071737ab2e49
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-03-07 20:15:29 +0530

    marks with data initialized

---
+++ basics/marks.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class marks {
+    public static void main(String args[]) {
+        int age = 16;
+        int physics = 97;
+        int chem = 98;
+        int maths = 95;
+
+        int marks[] = new int[3];
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 95;
+        System.out.println("physics = " + marks[0]);
+        System.out.println("chem = " + marks[1]);
+        System.out.println("maths = " + marks[2]);
+    }
+
+}
