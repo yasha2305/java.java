1fd2b60f97821726bbfe09376ce8bfa9be1fd5ea
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-03-08 12:16:27 +0530

    Volume of a box

---
+++ basics/volumeofBox.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class volumeofBox {
+    int length, width, heigth;
+
+    // constructor
+    volumeofBox(int l, int w, int h) {
+        length = l;
+        width = w;
+        heigth = h;
+    }
+
+    void volume() {
+        int v = length * width * heigth;
+        System.out.println("Volume = " + v);
+    }
+
+    public static void main(String args[]) {
+        volumeofBox b1 = new volumeofBox(2, 3, 4); // object 1
+        volumeofBox b2 = new volumeofBox(5, 6, 7); // object 2
+        b1.volume();
+        b2.volume();
+
+    }
+
+}
