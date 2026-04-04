4128454cdd7363074e8f8b5b13b6a2e4b84d8ea4
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-04-04 22:19:27 +0530

    Sum of digits

---
+++ basics/sumofdigit.java
@@ -0,0 +1,20 @@
+package basics;
+import java.util.Scanner;
+
+public class sumofdigit{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
