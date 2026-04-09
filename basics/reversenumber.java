19c086d24d3719352be5612ebdb811b37dae0743
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-04-09 23:47:24 +0530

    reverse a number

---
+++ basics/reversenumber.java
@@ -0,0 +1,18 @@
+package basics;
+import java.util.Scanner;
+
+public class reversenumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int rev = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Reverse = " + rev);
+    }
+}
